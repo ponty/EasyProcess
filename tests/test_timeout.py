3ec72b6e624855b880c56d3fe6ff68aa5108c9db
ponty <titi@pince.(none)>
2011-04-21 08:53:05 +0200

    new tests

--- tests/test_timeout.py
+++ tests/test_timeout.py
@@ -1,5 +1,5 @@
 from easyprocess import EasyProcess
-from nose.tools import eq_
+from nose.tools import eq_, timed
 from unittest import TestCase
 
 
@@ -17,3 +17,22 @@ class Test(TestCase):
         eq_(EasyProcess('sleep 0.3').call(timeout=0.1).return_code==0, False)
         eq_(EasyProcess('sleep 0.3').call(timeout=1).return_code==0, True)
     
+    @timed(2)
+    def test_time_cli1(self):
+        p=EasyProcess(['python', '-c', "import logging;logging.basicConfig(level=logging.DEBUG);from easyprocess import EasyProcess;EasyProcess('sleep 15').start()"])
+        p.call()
+        eq_(p.return_code,0)
+
+    @timed(2)
+    def test_time_cli2(self):
+        p=EasyProcess(['python', '-c', "import logging;logging.basicConfig(level=logging.DEBUG);from easyprocess import EasyProcess;EasyProcess('sleep 15').call(timeout=0.5)"])
+        p.call()
+        eq_(p.return_code,0)
+    
+    @timed(1.2)
+    def test_time2(self):
+        EasyProcess('sleep 5').call(timeout=1)
+        
+    @timed(0.3)
+    def test_time3(self):
+        EasyProcess('sleep 5').start()
