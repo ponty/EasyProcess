76e90ba715531d32084cd2e0960250e204dc248c
ponty <titi@pince.(none)>
2011-11-13 09:26:56 +0100

    test

--- tests/fast/test_timeout.py
+++ tests/fast/test_timeout.py
@@ -20,7 +20,7 @@ class Test(TestCase):
         eq_(EasyProcess('sleep 0.3').call().timeout_happened, False)
         eq_(EasyProcess('sleep 0.3').call(timeout=0.1).timeout_happened, True)
         eq_(EasyProcess('sleep 0.3').call(timeout=1).timeout_happened, False)
-        
+
     @timed(2)
     def test_time_cli1(self):
         p=EasyProcess(['python', '-c', "import logging;logging.basicConfig(level=logging.DEBUG);from easyprocess import EasyProcess;EasyProcess('sleep 5').start()"])
@@ -35,8 +35,20 @@ class Test(TestCase):
     
     @timed(1.2)
     def test_time2(self):
-        EasyProcess('sleep 5').call(timeout=1)
+        p=EasyProcess('sleep 5').call(timeout=1)
+        eq_(p.is_alive(), False)
+        eq_(p.timeout_happened, True)
+        eq_(p.return_code, -15)
+        eq_(p.stdout, '')
         
+    @timed(1.2)
+    def test_timeout_out(self):
+        p=EasyProcess(['python', '-c', "import time;print 'start';time.sleep(5);print 'end'"]).call(timeout=1)
+        eq_(p.is_alive(), False)
+        eq_(p.timeout_happened, True)
+        eq_(p.return_code, -15)
+        eq_(p.stdout, '')
+
     @timed(0.3)
     def test_time3(self):
         EasyProcess('sleep 5').start()
