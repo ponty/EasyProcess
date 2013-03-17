ad28dccbd8c6a9f5ef52a9b4879e53b93ec70fa3
ponty <ponty@home>
2013-03-17 19:58:10 +0100

    simplified pavement tasks

--- pavement.py
+++ pavement.py
@@ -8,7 +8,6 @@ from paved.util import *
 from paved.docs import *
 from paved.pycheck import *
 from paved.pkg import *
-from sphinxcontrib import paverutils
 from setuptools import find_packages
 
 # get info from setup.py
@@ -22,10 +21,10 @@ options(
         docroot='docs',
         builddir="_build",
     ),
-    pdf=Bunch(
-        builddir='_build',
-        builder='latex',
-    ),
+#    pdf=Bunch(
+#        builddir='_build',
+#        builder='latex',
+#    ),
 )
 
 
@@ -51,37 +50,16 @@ options.paved.dist.manifest.include.add('requirements.txt')
 
 @task
 @needs(
-    #           'clean',
     'sloccount',
     'html',
     'pdf',
     'sdist',
-    'nose', 'tox',
+    'nose', 
+    'tox',
 )
 def alltest():
     'all tasks to check'
     pass
-
-
-@task
-@needs('sphinxcontrib.paverutils.html')
-def html():
-    pass
-
-
-@task
-@needs('sphinxcontrib.paverutils.pdf')
-def pdf():
-    fpdf = list(path('docs/_build/latex').walkfiles('*.pdf'))[0]
-    d = path('docs/_build/html')
-    d.makedirs()
-    fpdf.copy(d)
-
-
-@task
-def tox():
-    '''Run tox.'''
-    sh('tox')
 
 
 @task
