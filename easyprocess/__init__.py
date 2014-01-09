e8838bfc77c1b688e904dd5bdcc20db30e99cad7
ponty <ponty@home>
2014-01-09 16:10:06 +0100

    make repr shorter

--- easyprocess/__init__.py
+++ easyprocess/__init__.py
@@ -133,7 +133,7 @@ class EasyProcess():
             self.cmd[0] = self.alias
 
     def __repr__(self):
-        msg = '<%s cmd_param=%s alias={alias} cmd=%s ({scmd}) oserror=%s returncode=%s stdout="%s" stderr="%s" timeout=%s>' % (
+        msg = '<%s cmd_param=%s cmd=%s oserror=%s returncode=%s stdout="%s" stderr="%s" timeout=%s>' % (
             self.__class__.__name__,
             self.cmd_param,
             self.cmd,
