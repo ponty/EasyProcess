61e32f27c026be048e7ce17bba6578422ec618ae
ponty <ponty@home>
2015-10-22 12:10:30 +0200

    drop support for py32

--- setup.py
+++ setup.py
@@ -37,9 +37,10 @@ classifiers = [
     "Programming Language :: Python :: 3",
     #    "Programming Language :: Python :: 3.0",
 #     "Programming Language :: Python :: 3.1",
-    "Programming Language :: Python :: 3.2",
+#     "Programming Language :: Python :: 3.2",
     "Programming Language :: Python :: 3.3",
     "Programming Language :: Python :: 3.4",
+#     "Programming Language :: Python :: 3.5",
 ]
 
 
