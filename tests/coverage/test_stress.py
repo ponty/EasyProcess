e359038f485d1991f0138d6041a5289269a51ef3
ponty <ponty@home>
2016-09-30 16:11:59 +0200

    fix test coverage py35 error: "Fatal Python error: deallocating None"

---
+++ tests/coverage/test_stress.py
@@ -0,0 +1,16 @@
+from easyprocess import EasyProcess, EasyProcessError
+from nose.tools import eq_
+
+def test_call():
+    for x in range(1000):
+        # test for:
+        # OSError exception:[Errno 24] Too many open files
+        print('index=', x)
+        eq_(EasyProcess('echo hi').call().return_code, 0)
+
+#    def test_start(self):
+#        for x in range(1000):
+#            print('index=', x)
+#            EasyProcess('echo hi').start()
+
+
---
+++ tests/nocoverage/test_stress.py
@@ -0,0 +1,13 @@
+from easyprocess import EasyProcess, EasyProcessError
+from nose.tools import eq_, timed
+from nose.tools import assert_not_equal as neq_
+
+# if run with coverage:
+#        Fatal Python error: deallocating None
+
+@timed(1000)
+def test_timeout(): # pragma: no cover
+        for x in range(1000):
+            print('index=', x)
+            neq_(   EasyProcess('sleep 5').call(timeout=0.05).return_code, 0)
+
