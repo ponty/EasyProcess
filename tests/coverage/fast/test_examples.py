99877c1f7aa6f7fe35d4433dfad1449999beef37
ponty <ponty@home>
2019-12-03 14:40:12 +0100

    skip testing examples on Windows (#18)

--- tests/coverage/fast/test_examples.py
+++ tests/coverage/fast/test_examples.py
@@ -1,10 +1,11 @@
 from easyprocess import EasyProcess
 from nose.tools import eq_
-from nose.tools import ok_
 import sys
 
-
 def test():
+    # skip these tests for Windows
+    if not sys.platform.startswith('linux'):
+        return
     eq_(EasyProcess([sys.executable, '-m', 'easyprocess.examples.ver']
                     ).call().return_code, 0)
     eq_(EasyProcess([sys.executable, '-m', 'easyprocess.examples.log']
