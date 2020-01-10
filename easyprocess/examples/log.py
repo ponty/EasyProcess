d0078635270dac3b7ad4e1536d7105e581f5a8db
ponty <ponty@home>
2020-01-10 13:49:47 +0100

    remove 'check()' from example

--- easyprocess/examples/log.py
+++ easyprocess/examples/log.py
@@ -6,13 +6,3 @@ logging.basicConfig(level=logging.DEBUG)
 
 EasyProcess('python --version').call()
 EasyProcess('ping localhost').start().sleep(1).stop()
-EasyProcess('python --version').check()
-try:
-    EasyProcess('bad_command').check()
-except Exception as detail:
-    print detail
-
-try:
-    EasyProcess('sh -c bad_command').check()
-except Exception as detail:
-    print detail
