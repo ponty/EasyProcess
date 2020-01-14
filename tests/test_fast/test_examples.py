f68089061fe7b64ce2fd9de02ec47dc76c311c2c
ponty <ponty@home>
2020-01-14 11:33:04 +0100

    comment

--- tests/test_fast/test_examples.py
+++ tests/test_fast/test_examples.py
@@ -3,9 +3,9 @@ from nose.tools import eq_
 import sys
 
 def test():
-    # skip these tests for Windows
+    # skip these tests for Windows/Mac
     if not sys.platform.startswith('linux'):
-        return
+        return    
     eq_(EasyProcess([sys.executable, '-m', 'easyprocess.examples.ver']
                     ).call().return_code, 0)
     eq_(EasyProcess([sys.executable, '-m', 'easyprocess.examples.log']
